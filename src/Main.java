0e3d8c9325eca78b56c5235f31a3014789d0b618
AliJohnnaton <trofimov2002@mail.ru>
2025-03-02 20:09:27 +0300

    Added second message

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет Мир!");
         System.out.println('\n');
+        System.out.println("Hello World!");
+        System.out.println('\n');
 
         for (int i = 5; i >= 0; --i) {
             System.out.println("i = " + i);
