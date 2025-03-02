8f53e99604d14c15eb09f355381841174ee80734
AliJohnnaton <trofimov2002@mail.ru>
2025-03-02 19:55:03 +0300

    Message and cycle changed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        System.out.println("Привет Мир!");
         System.out.println('\n');
 
-        for (int i = 0; i <= 5; ++i) {
+        for (int i = 5; i >= 0; --i) {
             System.out.println("i = " + i);
         }
     }
